3e0638937fee1a7390cda3628ec62f92a551a32d
Joshua Truscott <joshuatruscott95@gmail.com>
2022-04-16 16:27:35 +1000

    dark theme fix

--- src/theming/dark-theme.ts
+++ src/theming/dark-theme.ts
@@ -1,9 +1,9 @@
 import {DefaultTheme} from 'styled-components';
 import darkTokens from "./tokens-output/dark.json";
 
-export const lightTheme: DefaultTheme = {
+export const darkTheme: DefaultTheme = {
   "name": "Dark theme",
   ...darkTokens
 };
 
-export default lightTheme;
+export default darkTheme;
