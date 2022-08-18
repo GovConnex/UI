05995e9ec8a2be7d1ab29e637d1c3996698f3744
Simon Bracegirdle <sbracegirdle@gmail.com>
2022-08-18 10:18:32 +0800

    Update button and menu components to match new design token changes
    Bump UI library version

--- src/components/Button/Button.styles.ts
+++ src/components/Button/Button.styles.ts
@@ -1,64 +1,93 @@
 import styled from "styled-components";
-import { ButtonVariant } from "./Button";
+import { ButtonVariant, ButtonShape } from "./Button";
 import { Spacing } from "../../theming/global-theme.interface";
 
 const StyledAdornment = styled.span``;
 
 const StyledButton = styled.button<{
   variant: ButtonVariant;
+  iconOnly?: boolean;
+  iconOnlySize: keyof Spacing;
+  shape: ButtonShape;
   size: keyof Spacing;
 }>`
   cursor: pointer;
-  padding: ${props => `${props.theme.spacing[props.size]} ${props.theme.spacing.md}`};
-  border-radius: ${props => props.theme.borderRadius.xs};
+  padding: ${(props) =>
+    `${props.theme.spacing[props.size]} ${props.theme.spacing.md}`};
+  border-radius: ${(props) =>
+    props.shape === "rect" || !props.iconOnly
+      ? props.theme.borderRadius.xs
+      : "100%"};
   border: 0 solid transparent;
   display: flex;
-  gap: ${props => props.theme.spacing.sm};
+  gap: ${(props) => props.theme.spacing.sm};
 
-  &:disabled {
-    background-color: ${props => props.theme.primary.neutral["300"]};
+  ${({ iconOnly, theme, iconOnlySize }) =>
+    iconOnly
+      ? `
+      width: ${theme.spacing[iconOnlySize]};
+      height: ${theme.spacing[iconOnlySize]};
+      align-items: center;
+      justify-content: center;
+      padding: 0;
+      `
+      : ""}
+
+  &:active:not(:disabled) {
+    outline: ${({ theme }) =>
+      `${theme.borderWidth.lg} solid ${theme.core.border.borderFocus};`}
+    outline-offset: ${({ theme }) => `${theme.borderWidth.lg};`}
   }
 
-  &:disabled:hover {
-    background-color: ${props => props.theme.primary.neutral["300"]};
-  }
-
-  ${({theme, variant}) => variant === "primary" && `
-    color: ${theme.primary.neutral["50"]};
-    background-color: ${theme.primary.base.brand};
+  ${({ theme, variant }) =>
+    variant === "primary" &&
+    `
+    color: ${theme.core.content.contentInversePrimary};
+    background-color: ${theme.extended.state.primaryBase};
 
     &:hover {
-      background-color: ${theme.primary.brand["700"]};
+      background-color: ${theme.extended.state.primaryHover};
     }
 
     &:active {
-      background-color: ${theme.primary.brand["900"]};
+      background-color: ${theme.extended.state.primaryHover};
+    }
+
+    &:disabled {
+      background-color: ${theme.extended.state.disabled};
     }
   `}
 
-  ${({theme, variant}) => variant === "secondary" && `
-    color: ${theme.primary.neutral["900"]};
-    background-color: ${theme.primary.neutral["100"]};
+  ${({ theme, variant }) =>
+    variant === "secondary" &&
+    `
+    color: ${theme.core.content.contentSecondary};
+    background-color: ${theme.extended.state.secondaryBase};
+    border: ${theme.borderWidth.md} solid ${theme.core.border.borderLight};
 
     &:hover {
-      background-color: ${theme.primary.neutral["300"]};
+      background-color: ${theme.extended.state.secondaryHover};
     }
 
     &:active {
-      background-color: ${theme.primary.neutral["200"]};
+      background-color: ${theme.extended.state.secondaryBase};
+    }
+
+    &:disabled {
+      background-color: ${theme.extended.state.secondaryHover};
+      border-color: ${theme.extended.state.disabled};
+      color: ${theme.extended.state.disabled};
     }
   `}
 
-  ${({theme, variant}) => variant === "text" && `
+  ${({ theme, variant }) =>
+    (variant === "text" || variant === "tertiary") &&
+    `
     background-color: transparent;
-    color: ${theme.primary.neutral["900"]};
+    color: ${theme.core.content.contentSecondary};
 
-    &:hover {
-      background-color: ${theme.primary.neutral["100"]};
-    }
-
-    &:active {
-      background-color: ${theme.primary.neutral["200"]};
+    &:disabled {
+      color: ${theme.extended.state.disabled};
     }
   `}
 `;
