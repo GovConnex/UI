6f3b8b73df633f1705f402bf480968ef8743451e
Simon Bracegirdle <sbracegirdle@gmail.com>
2022-08-19 15:26:19 +0800

    Add forwardRef to card component. Bump and publish.

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -4,10 +4,16 @@ import StyledCard from "./Card.styles";
 export interface CardProps {
   focused?: boolean;
   children?: React.ReactNode;
-};
+}
 
-const Card = (props: CardProps) => {
-  return <StyledCard focused={!!props.focused}>{props.children}</StyledCard>;
-};
+const Card = React.forwardRef<HTMLDivElement, CardProps>(
+  (props: CardProps, ref) => {
+    return (
+      <StyledCard ref={ref} focused={!!props.focused}>
+        {props.children}
+      </StyledCard>
+    );
+  }
+);
 
 export default Card;
