a708b34b0a9b47f0d2e3507d5b901f7b436a9753
Symon <35018266+symonomicon@users.noreply.github.com>
2023-03-03 10:05:40 +0800

    feat; Add newsletter modal (#58)
    * feat; Add newsletter modal
    
    * ref(newsletter): Remove component entirely, move to webapp
    
    * ref; bump ui version

--- src/components/Icon/Icon.tsx
+++ src/components/Icon/Icon.tsx
@@ -7,9 +7,10 @@ import { fas } from "@fortawesome/pro-solid-svg-icons";
 import { far } from "@fortawesome/pro-regular-svg-icons";
 import { fal } from "@fortawesome/pro-light-svg-icons";
 import { fad } from "@fortawesome/pro-duotone-svg-icons";
+import { fat } from "@fortawesome/pro-thin-svg-icons"
 import { library } from "@fortawesome/fontawesome-svg-core";
 
-library.add(fas, far, fal, fad);
+library.add(fas, far, fal, fad, fat);
 
 const Icon = (props: FontAwesomeIconProps) => {
   return <FontAwesomeIcon {...props} />;
--- src/components/Popover/Popover.tsx
+++ src/components/Popover/Popover.tsx
@@ -1,6 +1,6 @@
-import React, { useState } from "react";
-import StyledPopover from "./Popover.styles";
-import { Placement, PositioningStrategy }from '@popperjs/core';
+import React, { useState } from 'react';
+import StyledPopover from './Popover.styles';
+import { Placement, PositioningStrategy } from '@popperjs/core';
 import { usePopper, Modifier } from 'react-popper';
 
 export interface PopoverProps extends React.HTMLAttributes<HTMLDivElement> {
@@ -9,21 +9,21 @@ export interface PopoverProps extends React.HTMLAttributes<HTMLDivElement> {
   modifiers?: ReadonlyArray<Modifier<any>>;
   placement?: Placement;
   strategy?: PositioningStrategy;
-};
+}
 
 const Popover = React.forwardRef<HTMLSpanElement, PopoverProps>((props: PopoverProps, ref) => {
   const [popperElement, setPopperElement] = useState<HTMLDivElement | null>(null);
   const { styles, attributes } = usePopper(props.anchorEl.current, popperElement, {
     modifiers: props.modifiers,
     placement: props.placement,
-    strategy: props.strategy
+    strategy: props.strategy,
   });
 
   return (
-    <span ref={ref} style={{ display: "contents" }}>
+    <span ref={ref} style={{ display: 'contents' }}>
       <StyledPopover
         ref={setPopperElement}
-        style={styles.popper}
+        style={{ ...props.style, ...styles.popper }}
         {...attributes.popper}
       >
         {props.children}
