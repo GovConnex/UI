564c4678af86134551004b147cb7425cca6fffb5
Dawn Carrasco <dawncarrasco@Dawns-MacBook-Pro.local>
2023-07-28 14:44:14 +0800

    chore: fix test issues

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,11 @@
 module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
     "extends": [
         "eslint:recommended",
-        "plugin:@typescript-eslint/recommended",
-        "plugin:storybook/recommended"
+        "plugin:@typescript-eslint/recommended"
     ],
     "overrides": [
         {
@@ -19,6 +22,7 @@ module.exports = {
     ],
     "parser": "@typescript-eslint/parser",
     "parserOptions": {
+        "ecmaVersion": "latest",
         "sourceType": "module"
     },
     "plugins": [
