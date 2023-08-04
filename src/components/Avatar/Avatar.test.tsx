f1d06ab30808f9cb685f71ac821b068b398adc68
Simon Bracegirdle <60430071+sbracegirdle@users.noreply.github.com>
2023-08-04 10:27:43 +0800

    Feat/improve avatar to support bills inquiries (#84)
    * Feat; improve and fix Avatar to support bills and inquiries
    
    * Fix; bump version and limit initials to two characters

--- src/components/Avatar/Avatar.test.tsx
+++ src/components/Avatar/Avatar.test.tsx
@@ -1,10 +1,63 @@
 import React from "react";
 import {render} from "../test-utils";
 
-import Avatar from "./Avatar";
+import Avatar, {getInitials} from "./Avatar";
 
 describe("Avatar", () => {
-  test("renders the Avatar component", () => {
-    render(<Avatar />);
+  it("should render an image when src is provided and no error", () => {
+    const {getByAltText} = render(<Avatar alt="John Doe" src="path/to/image.jpg" />);
+    expect(getByAltText("John Doe")).toBeInTheDocument();
+  });
+
+  it("should display initials when src is not provided", () => {
+    const {getByText} = render(<Avatar alt="John Doe" />);
+    expect(getByText("JD")).toBeInTheDocument();
+  });
+
+  it("should display a question mark when no alt text is provided", () => {
+    const {getByText} = render(<Avatar />);
+    expect(getByText("?")).toBeInTheDocument();
+  });
+
+  it("should display initials when src results in an error", () => {
+    const {getByText} = render(<Avatar alt="John Doe" src="path/to/invalid.jpg" />);
+    // Simulate image load error
+    const image = document.querySelector("img");
+    if (image) {
+      const errorEvent = new Event("error");
+      image.dispatchEvent(errorEvent);
+    }
+    expect(getByText("JD")).toBeInTheDocument();
+  });
+
+  it("should have the appropriate size", () => {
+    const {container} = render(<Avatar alt="John Doe" size="md" />);
+    expect(container.firstChild).toHaveStyle("width: 18px");
+  });
+
+  it("should have the appropriate shape", () => {
+    const {container} = render(<Avatar alt="John Doe" variant="circle" />);
+    expect(container.firstChild).toHaveStyle("border-radius: 50%");
+  });
+
+  describe("getInitials", () => {
+    describe("getInitials function", () => {
+      it.each([
+        ["Defence — Cocos (Keeling) Islands Airfield Upgrade", "DC"],
+        ["?", "?"],
+        ["John Doe", "JD"],
+        [undefined, ""],
+        ["", ""],
+        [" ", ""],
+        ["           ", ""],
+        ["John Michael Doe", "JM"],
+        ["Vincent van Gogh", "VG"],
+        ["Committee of Privileges and Members' Interests", "CP"],
+        ["john doe", "JD"],
+        ["John", "J"],
+      ])("should return '%s' when input is '%s'", (input, expected) => {
+        expect(getInitials(input)).toBe(expected);
+      });
+    });
   });
 });
