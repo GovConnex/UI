675075e72385db8149e72cca90c4fd359580304d
Dawn Carrasco <dawncarrasco@Dawns-MacBook-Pro.local>
2023-08-07 16:54:32 +0800

    chore: updated datatable description

--- src/components/DataTable/ComponentSummary.tsx
+++ src/components/DataTable/ComponentSummary.tsx
@@ -1,10 +1,10 @@
 const componentSummary = `
 ##Summary
-The Background icon component may contain various icons to visualize or to give information as an image.
+The datatable is a structured presentation of data in rows and columns, often used for organizing and displaying tabular information. It enables efficient data analysis, sorting, and comparison, for visualizing and managing datasets in a structured format.
 ##Purpose
-The Background component was added to create a visual of the information it is trying to portray.
+The datatable is used for streamlining data presentation, enabling users to make informed decisions, perform data-driven tasks, and gain insights from large datasets with ease.
 ##Figma Link
-https://www.figma.com/file/yJ8kQwyMxuTZudD90MxgOU/%F0%9F%93%90-Components-and-Patterns?type=design&node-id=103-320&mode=design&t=QFaUGXlsf04huhew-0
+https://www.figma.com/file/yJ8kQwyMxuTZudD90MxgOU/%F0%9F%93%90-Components-and-Patterns?type=design&node-id=178-4324&mode=design&t=HF4rJmb3tYWcqZX1-0
 `;
 
 export default componentSummary;
